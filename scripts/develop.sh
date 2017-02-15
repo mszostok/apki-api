4142e6adbd2f2d52a6de10b5012ffbe68de086ed
Mateusz Szostok <mateusz.szostok@sap.com>
2017-02-15 18:56:56 +0100

    Update application executing scripts to support helper script to setting env vars

--- scripts/develop.sh
+++ scripts/develop.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+my_dir="$(dirname "$0")"
+
 # firstly run flow check
 ./node_modules/flow-bin/vendor/flow
 
 if [ $? -eq 0 ]; then
   # if flow check passed run node
   echo "----------"
+  source $my_dir/set_envs.sh
   node index.js
 else
   # else exit with code 0 (nodemon will still run)
---
+++ scripts/set_envs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#export all required env variables
+
+echo "Env vars was set"
