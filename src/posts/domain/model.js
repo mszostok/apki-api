d9231a6adccc9e1847c5e9bb8a41e19475c7ea66
Mateusz Szostok <mateusz.szostok@sap.com>
2017-02-19 02:09:58 +0100

    Add validators for schemas

--- src/posts/domain/model.js
+++ src/posts/domain/model.js
@@ -1,12 +1,19 @@
 import mongoose from 'mongoose';
 
 const postSchema = mongoose.Schema({
-  title: String,
+  title: {
+    type: String,
+    require: true,
+  },
+  content: {
+    type: String,
+    require: true,
+  },
   author: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
+    require: true,
   },
-  content: String,
 });
 
 const PostModel = mongoose.model('Post', postSchema);
--- src/users/domain/model.js
+++ src/users/domain/model.js
@@ -1,12 +1,32 @@
 import mongoose from 'mongoose';
 
+const supportRoles = 'admin|moderator|teacher|students';
+const ruleRegex = new RegExp(`/[${supportRoles}]\b/`);
 
 const userSchema = mongoose.Schema({
-  nickname: String,
-  email: String,
+  nickname: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   uid: String,
   password: String,
-  roles: [String],
+  roles: {
+    type: [String],
+    validate: {
+      validator(roles) {
+        const isRulesValid = roles.every(role => ruleRegex.test(role));
+
+        return isRulesValid;
+      },
+      message: `'{VALUE}' is not a valid roles! Supported rules are: ${supportRoles}`,
+    },
+  },
   posts: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Post',
